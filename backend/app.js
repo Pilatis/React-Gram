6f3b1ba078a473a90026cb94ecd7e95baeb3dee6
Lucas Pilati <pilatilucas@gmail.com>
2025-03-23 17:07:50 -0300

    created connection in data base mongoDB, intilized firsts schemas

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ require("dotenv").config()
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+const conn = require("./src/config/db.js")
 
 const port = process.env.PORT;
 
@@ -10,9 +11,17 @@ const app = express();
 
 // config JSON and form data response
 app.use(express.json())
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-const router = require("./models/controllers/routes/Router.js");
+// CORS
+app.use(cors({ credentials: true, origin: "http://localhost:3001" }));
+
+// upload directory
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+const router = require("./src/routes/Router.js");
+
+//conn();
 
 app.use(router);
 
